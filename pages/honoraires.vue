99185525c8a4ce277629a8d5addfcb972eb12a61
Chaloulou75 <c.jeandey@gmail.com>
2022-11-20 19:11:46 +0100

    try to load swiper on mounted

--- pages/honoraires.vue
+++ pages/honoraires.vue
@@ -7,81 +7,99 @@
     >
       Honoraires
     </h1>
-    <section class="w-full mx-auto my-4 md:px-6">
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        La fixation du montant des honoraires est libre.
-      </p>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Les honoraires sont fixés selon les usages, en fonction de la situation
-        de fortune du client, de la difficulté de l’affaire, des frais exposés
-        par l’avocat, de la notoriété et des diligences de celui-ci.
-      </p>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Une convention d'honoraires sera conclue entre vous et Maître Margaux
-        BENDELAC.
-      </p>
-      <h2
-        class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
-      >
-        Honoraires au temps passé
-      </h2>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Une rémunération à l’heure est convenue au début de votre affaire.
-      </p>
-      <h2
-        class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
-      >
-        Honoraires au forfait
-      </h2>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Une rémunération globale est fixée pour l’ensemble de la mission.
-      </p>
-      <h2
-        class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
-      >
-        Honoraires de résultat
-      </h2>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Un honoraires au temps passé ou au forfait de base, auquel une
-        rémunération supplémentaire sera ajoutée selon les bénéfices tirés du
-        procès.
-      </p>
-      <h2
-        class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
-      >
-        La contestation des honoraires d’avocat
-      </h2>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Vous avez la possibilité de saisir le médiateur de la consommation de la
-        profession d’avocat, qui vous offrira son aide afin de trouver une
-        solution à l’amiable ou de saisir le bâtonnier de l’ordre des avocats,
-        dans le cas où aucune solution à l’amiable n’aurait été trouvée.
-      </p>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Pour vous assister dans cette démarche, faites appel au médiateur dont
-        les coordonnées sont mentionnées ci-dessous:
-      </p>
-      <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
-        Carole Pascarel – Médiateur National de la Profession d’Avocat
-      </p>
-      <p class="text-xl text-justify text-black md:text-2xl">
-        180 boulevard Haussmann <br />
-        75008 Paris<br />
-        E-mail:
-        <span class="underline"
-          ><a href="mailto:mediateur@mediateur-consommation-avocat.fr">
-            mediateur@mediateur-consommation-avocat.fr</a
-          ></span
-        ><br />
-        Site Internet:
-        <a
-          href="https://mediateur-consommation-avocat.fr"
-          rel="noreferrer"
-          target="_blank"
-          class="underline"
-          >https://mediateur-consommation-avocat.fr
-        </a>
-      </p>
+    <section
+      class="flex flex-col w-full mx-auto my-4 space-y-8 md:space-y-12 md:px-6"
+    >
+      <div>
+        <h2
+          class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
+        >
+          Fixation des honoraires
+        </h2>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          La fixation du montant des honoraires est libre.
+        </p>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Les honoraires sont fixés selon les usages, en fonction de la
+          situation de fortune du client, de la difficulté de l’affaire, des
+          frais exposés par l’avocat, de la notoriété et des diligences de
+          celui-ci.
+        </p>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Une convention d'honoraires sera conclue entre vous et Maître Margaux
+          BENDELAC.
+        </p>
+      </div>
+      <div>
+        <h2
+          class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
+        >
+          Honoraires au temps passé
+        </h2>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Une rémunération à l’heure est convenue au début de votre affaire.
+        </p>
+      </div>
+      <div>
+        <h2
+          class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
+        >
+          Honoraires au forfait
+        </h2>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Une rémunération globale est fixée pour l’ensemble de la mission.
+        </p>
+      </div>
+      <div>
+        <h2
+          class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
+        >
+          Honoraires de résultat
+        </h2>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Un honoraires au temps passé ou au forfait de base, auquel une
+          rémunération supplémentaire sera ajoutée selon les bénéfices tirés du
+          procès.
+        </p>
+      </div>
+      <div>
+        <h2
+          class="mb-3 text-2xl font-semibold text-justify text-black md:text-3xl md:mb-5"
+        >
+          La contestation des honoraires d’avocat
+        </h2>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Vous avez la possibilité de saisir le médiateur de la consommation de
+          la profession d’avocat, qui vous offrira son aide afin de trouver une
+          solution à l’amiable ou de saisir le bâtonnier de l’ordre des avocats,
+          dans le cas où aucune solution à l’amiable n’aurait été trouvée.
+        </p>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Pour vous assister dans cette démarche, faites appel au médiateur dont
+          les coordonnées sont mentionnées ci-dessous:
+        </p>
+        <p class="mb-3 text-xl text-justify text-black md:text-2xl md:mb-5">
+          Carole Pascarel – Médiateur National de la Profession d’Avocat
+        </p>
+        <p class="text-xl text-justify text-black md:text-2xl">
+          180 boulevard Haussmann <br />
+          75008 Paris<br />
+          E-mail:
+          <span class="underline"
+            ><a href="mailto:mediateur@mediateur-consommation-avocat.fr">
+              mediateur@mediateur-consommation-avocat.fr</a
+            ></span
+          ><br />
+          Site Internet:
+          <a
+            href="https://mediateur-consommation-avocat.fr"
+            rel="noreferrer"
+            target="_blank"
+            class="underline"
+            >https://mediateur-consommation-avocat.fr
+          </a>
+        </p>
+      </div>
     </section>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Autoplay } from "swiper";
-
-// Import Swiper styles
 import "swiper/css";
 
-const modules = [Autoplay];
+const modules = ref([Autoplay]);
 </script>
 
 <template>
@@ -13,12 +12,12 @@ const modules = [Autoplay];
     class="flex items-center justify-center flex-1 w-full h-full px-2 mx-auto bg-gray-500 bg-center bg-no-repeat bg-cover md:px-8 bg-hero-colonnes bg-blend-overlay"
   >
     <swiper
-      :spaceBetween="30"
+      :slides-per-view="1"
+      :space-between="30"
       :loop="true"
       :centeredSlides="true"
       :autoplay="{
         delay: 2600,
-        disableOnInteraction: false,
       }"
       :modules="modules"
       class="mySwiper"
@@ -46,13 +45,4 @@ const modules = [Autoplay];
       </swiper-slide>
     </swiper>
   </div>
-  <!-- <div
-    class="flex items-center justify-center flex-1 w-full h-full px-2 mx-auto bg-gray-500 bg-center bg-no-repeat bg-cover md:px-8 bg-hero-colonnes bg-blend-overlay"
-  >
-    <h1
-      class="max-w-xl mx-auto text-3xl font-medium text-center text-white md:text-5xl mix-blend-lighten font-cormorant"
-    >
-      Avocate en Droit pénal, Droit pénal des affaires, Droit international
-    </h1>
-  </div> -->
 </template>
