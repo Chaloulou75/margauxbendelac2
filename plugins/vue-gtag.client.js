a6e0564513b2706dd2d5793d7e256f7bd39a45d3
Chaloulou75 <c.jeandey@gmail.com>
2023-02-02 13:15:11 +0100

    try vue gtag test2

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,8 +1,12 @@
 import VueGtag from "vue-gtag";
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: "G-JF1NEH7R9K",
+  nuxtApp.vueApp.use(
+    VueGtag,
+    {
+      config: {
+        id: "G-JF1NEH7R9K",
+      },
     },
-  });
+    nuxtApp.$router
+  );
 });
