1209b4c7c8617808a44eddc31a6ba38ce55f48df
Chaloulou75 <c.jeandey@gmail.com>
2024-03-06 19:51:56 +0100

    phone mobile2

--- components/Phone.vue
+++ components/Phone.vue
@@ -3,12 +3,12 @@ import { PhoneIcon } from "@heroicons/vue/24/outline";
 </script>
 <template>
   <div
-    class="fixed z-20 flex items-center space-x-4 group bottom-7 md:bottom-4 right-4"
+    class="fixed z-20 flex items-center space-x-4 group bottom-10 md:bottom-4 right-4"
   >
     <div
       class="px-2 py-1.5 bg-green-500 group-hover:bg-green-600 text-sm text-white rounded-xl"
     >
-      Urgence pénale: + 33 7 62 35 22 98
+      Urgence pénale
     </div>
     <div
       class="flex items-center justify-center p-4 bg-green-500 rounded-full group-hover:bg-green-600"
