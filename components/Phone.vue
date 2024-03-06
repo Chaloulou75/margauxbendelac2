b23de90910acffdb56b95718806739365eebc812
Chaloulou75 <c.jeandey@gmail.com>
2024-03-06 17:55:48 +0100

    phone mobile

--- components/Phone.vue
+++ components/Phone.vue
@@ -2,7 +2,9 @@
 import { PhoneIcon } from "@heroicons/vue/24/outline";
 </script>
 <template>
-  <div class="fixed z-20 flex items-center space-x-4 group bottom-4 right-4">
+  <div
+    class="fixed z-20 flex items-center space-x-4 group bottom-7 md:bottom-4 right-4"
+  >
     <div
       class="px-2 py-1.5 bg-green-500 group-hover:bg-green-600 text-sm text-white rounded-xl"
     >
