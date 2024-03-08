c950190f7badcadd49e11eb08dfba4d2ef084865
Chaloulou75 <c.jeandey@gmail.com>
2024-03-08 23:12:15 +0100

    color phone and moved ids

--- components/Phone.vue
+++ components/Phone.vue
@@ -2,14 +2,16 @@
 import { PhoneIcon } from "@heroicons/vue/24/outline";
 </script>
 <template>
-  <div class="fixed z-20 flex items-center space-x-4 group bottom-20 right-4">
+  <div
+    class="fixed z-20 flex items-center space-x-2 md:space-x-4 group bottom-16 md:bottom-20 right-4"
+  >
     <div
-      class="px-3 py-2 text-sm text-white bg-green-600 rounded-2xl group-hover:bg-green-700"
+      class="px-3 py-2 text-sm text-white bg-gray-900/90 rounded-2xl group-hover:bg-gray-900"
     >
       Urgence pénale
     </div>
     <div
-      class="flex items-center justify-center p-4 bg-green-600 rounded-full group-hover:bg-green-700"
+      class="flex items-center justify-center p-4 rounded-full bg-gray-900/90 group-hover:bg-gray-900"
     >
       <a href="tel:+33762352298"><PhoneIcon class="w-6 h-6 text-white" /></a>
     </div>
